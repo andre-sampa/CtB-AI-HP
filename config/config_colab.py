a3b2bc78c367ae655d6a7f64417895c188f33b58
Andre <andrefranceschini@gmail.combitcoind>
2025-01-25 07:48:10 +0100

    Update

--- config/config_colab.py
+++ config/config_colab.py
@@ -12,11 +12,4 @@ else:
     print("=== Debug: Success ===")
     print("Hugging Face token loaded successfully.")
 
-# List of models with aliases
-models = [
-    {"alias": "FLUX.1-dev", "name": "black-forest-labs/FLUX.1-dev"},
-]
-
-# Debugging: Print prompt and model options
-print("Model Options:", [m["alias"] for m in models])
 
---
+++ config/models.py
@@ -0,0 +1,7 @@
+# List of models with aliases
+models = [
+    {"alias": "FLUX.1-dev", "name": "black-forest-labs/FLUX.1-dev"},
+]
+
+# Debugging: Print prompt and model options
+print("Model Options:", [m["alias"] for m in models])
