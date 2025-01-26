59c1ef902818ff0478a3fdd832ce270441b1323c
Andre <andrefranceschini@gmail.combitcoind>
2025-01-26 03:32:42 +0100

    “Update”

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from src.gradio_interface import demo
 
-# Initialize the InferenceClient with the default model
-#client = InferenceClient(models[0]["name"], token=api_token)
-
 # Launch the Gradio app
 demo.launch()
